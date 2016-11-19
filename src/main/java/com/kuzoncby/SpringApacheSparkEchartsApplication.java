aabf634416bef83a20753ba682d30bbe351a5fa6
kuzoncby <kuzoncby@gmail.com>
2016-11-19 12:46:22 +0800

    init repo

---
+++ src/main/java/com/kuzoncby/SpringApacheSparkEchartsApplication.java
@@ -0,0 +1,12 @@
+package com.kuzoncby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApacheSparkEchartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApacheSparkEchartsApplication.class, args);
+	}
+}
